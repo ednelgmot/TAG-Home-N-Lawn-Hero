8f65e3aae8ff08ca850fb663a44e4e9028cee4ec
Thomas Glende <thomas.glende@cpcc.edu>
2024-02-26 21:25:40 -0500

    Adding Forms and views, updating table

--- handl/models.py
+++ handl/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# Use to classify an event type
+EVENT_CHOICES = (
+    ('S', 'Seasonal'),
+    ('A', 'Annual'),
+    ('M', 'Maintenance'),
+)
+
+# Use to create multiple instances of an event/task
+REPEAT_CHOICES =  (
+    ('Y','Yes'),
+    ('N','No'),
+)
+
+MONTH_CHOICES = [
+        ('January', 'January'),
+        ('February', 'February'),
+        ('March', 'March'),
+        ('April', 'April'),
+        ('May', 'May'),
+        ('June', 'June'),
+        ('July', 'July'),
+        ('August', 'August'),
+        ('September', 'September'),
+        ('October', 'October'),
+        ('November', 'November'),
+        ('December', 'December'),
+    ]
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    tasktype = models.CharField('Type of Task',max_length=2, choices=EVENT_CHOICES, null=True, blank=True,
+                                            help_text="Classify the event")
+    repeatevent = models.CharField('Task repeats?', max_length=2, choices=REPEAT_CHOICES, null=True, blank=True,
+                                            help_text="Select if the event repeats or not")
+    moyval = models.CharField('Month it repeats',max_length=9, choices=MONTH_CHOICES, null=True, blank=True,
+                                            help_text="Select Month event repeats in")
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        order_with_respect_to = 'user'   
--- handl/views.py
+++ handl/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
 
 # Create your views here.
 from django.http import HttpResponse
 
+class TaskList(ListView):
+   model = Task
+   context_object_name = 'tasks'
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the H and L index.")
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'handl/task_detail.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = ['user','title','tasktype','description','repeatevent','moyval','complete']
+    success_url = reverse_lazy('tasks')
+
+   # def form_valid(self, form):
+   #     form.instance.user = self.request.user
+   #     return super(TaskCreate, self).form_valid(form)
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'description','complete']
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
